14eaeddf0f81d38466ca6ea4ee2cc9101a09ffcf
Paul Gibler <paul.gibler@gmail.com>
2014-07-31 16:25:49 -0400

    Finished the Solver portion of the project, which uses backtracking to solve the Sudoku puzzle.
    Updated application to properly invoke the Solver.

--- src/application.rb
+++ src/application.rb
@@ -1,14 +1,27 @@
 # #Application uses the #Solver to move the #BoardState as close to a solution as possible.
 
 require_relative 'solver/solver'
+require_relative 'provider/provider'
+require_relative 'provider/file_provider'
 
 # #Application takes in a set #provider and a hash of #options and returns a #BoardState, if possible.
 class Application
 
+  DEFAULT_PROVIDER=FileProvider
+
   # Runs the #Application by getting the #BoardState from the #Provider and then having #Solver.solve interact with it
   #
   # @provider - The #Provider used to get the #BoardState
-  def self.run!(provider)
-    Solver.solve provider.provide_puzzle_state
+  def self.run!(provider_type)
+    Solver.solve provider_type
+  end
+
+  # Returns a provider class based on name, if available.
+  #
+  # @provider_name - The name of the requested provider class.
+  def self.get_provider_type(provider_name)
+    Provider.descendants.find do |clazz|
+      clazz.provider_name == provider_name
+    end
   end
 end
--- src/solver/solver.rb
+++ src/solver/solver.rb
@@ -1,35 +1,87 @@
-# #Solver is to be run with the options and solve a puzzle.
+# #Solver is to be run with the options and solve a board.
 class Solver
 
   # Determines if a #BoardState has a valid sudoku and generates a #BoardState containing that data.
+  # The #BoardState is retrieved from the provider.
   #
-  # @puzzle_state - A #BoardState instance that will be solved looking for a valid sudoku state for it, if possible.
-  def self.solve(puzzle_state)
-    # Contains the puzzle states, as they are executed.
-    # This gives us a history of our puzzle states.
-    puzzle_states_stack = []
+  # @provider_type - A #Provider type that supplies a #BoardState.
+  def self.solve(provider_type)
+    @provider = provider_type.new
 
-    # Put the current puzzle into the puzzle state stack.
-    puzzle_states_stack << puzzle_state
+    # Solve the puzzle
+    board_state, result = solve_sudoku(@provider.provide_board_state)
 
-    # Iterates through solving puzzle step-by-step.
-    while(!puzzle_state.incomplete?)
-
-      # Try to add a number to the #BoardState.
-      # #result is set to nil if try_to_add_number_fails.
-      puzzle_state = try_to_add_number puzzle_state
-
-      result.nil? ?
-          return nil : # Return nil for #solve_puzzle if #result.nil?
-          puzzle_states_stack << puzzle_state # Add the next #BoardState to the #puzzle_states_stack
-    end
-
-    # Return the puzzle state
-    puzzle_state
+    # The resulting #BoardState and #Provider will be returned.
+    return @provider, board_state, result
   end
 
-  # Tries to add a number to a #BoardState, if it is legal.
-  def try_to_add_number(puzzle_state)
+  private
 
+  # Solves sudoku. Returns the resulting #BoardState.
+  #
+  # @board_state - The current board state to check
+  def self.solve_sudoku(board_state)
+    # If there is no unassigned location, we are done
+    if (@provider.filled? board_state)
+      return board_state, true # The #board_state is complete. Done!
+    end
+
+    column, row = find_next_unassigned_location(board_state)
+
+    # Iterate through assignable values.
+    @provider.assignable_values.each do |num|
+
+      # If #num is not present at the unassigned #row and #column...
+      if (is_safe?(board_state, column, row, num))
+
+        # Make tentative assignment.
+        updated_board_state = board_state.update(column, row, num)
+
+        # Try and solve the next step. If this returns true,
+        # the #board_state is complete and we have succeeded.
+        result_board_state, operation_result = solve_sudoku(updated_board_state)
+
+        # If it succeeded, return the board_state
+        if (operation_result)
+          return result_board_state, true
+        else
+          next
+        end
+      end
+    end
+
+    # This triggers backtracking.
+    return board_state, false
+  end
+
+  # Returns true if the #board_state can be assigned the #num value at #row and #column.
+  # This is possible if that number does not appear in that row, column, or 3x3 subsection
+  # corresponding to that location
+  #
+  # @board_state - The #BoardState instance whose safety is being checked.
+  # @column - The column number to test for insertion safeness.
+  # @row - The row number to test for insertion safeness.
+  # @num - The number to insert into the board at (row, column).
+  def self.is_safe?(board_state, column, row, num)
+    if( !board_state.row(row).contains?(num) && # If the row doesn't have the value...
+        !board_state.column(column).contains?(num) && # ...and the column doesn't have the value...
+        !board_state.zone_at(column, row).contains?(num)) # ...and the zone doesn't have the value.
+      true
+    else
+      false
+    end
+  end
+
+  # Returns the next (row, column) pair that do not have a value assigned to them.
+  #
+  # @board_state - The board state to check
+  def self.find_next_unassigned_location(board_state)
+    board_state.each_cell do |column, row|
+      if @provider.is_value_unassigned?( board_state.at(column, row) )
+        return column, row
+      end
+    end
+
+    raise 'Board is complete, no empty locations could be found.'
   end
 end
