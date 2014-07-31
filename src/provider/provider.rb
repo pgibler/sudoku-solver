0e0701a92babdf0e57dcbe24899f3b8958922759
Paul Gibler <paul.gibler@gmail.com>
2014-07-31 16:42:53 -0400

    Updated comment in Provider.

--- src/provider/provider.rb
+++ src/provider/provider.rb
@@ -10,30 +10,26 @@ class Provider
 
   # The provide method
   def provide_board_state
-
   end
 
   # Determines if a cell is empty or not based on its value.
   #
   # @value - The value of the cell being checked.
   def is_value_unassigned?(value)
-
   end
 
   # Returns #true if the #board_state is filled. Otherwise, #false.
   #
   # @board_state - The #BoardState whose filled status is being determined.
   def filled?(board_state)
-
   end
 
   # Returns an #Array containing the values that could be assigned to a cell given this provider.
   # Unless the #Provider sub-type does something out of the ordinary, this will usually return (1..9).
   def assignable_values
-
   end
 
-  # Lets us get all descendants of the #Provider object.
+  # Gets all descendants of the #Provider object.
   def self.descendants
     ObjectSpace.each_object(Class).select { |klass| klass < self }
   end
