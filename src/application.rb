36abc56b85d4e6450a1cb2fbc3d8c195e5c718dd
Paul Gibler <paul.gibler@gmail.com>
2014-07-31 16:43:12 -0400

    Added in Solver system to sudoku-solver. Custom Solver types can be used for solving the provided sudoku puzzle in any way that is possible.

--- src/application.rb
+++ src/application.rb
@@ -1,6 +1,7 @@
 # #Application uses the #Solver to move the #BoardState as close to a solution as possible.
 
 require_relative 'solver/solver'
+require_relative 'solver/backtracking_solver'
 require_relative 'provider/provider'
 require_relative 'provider/file_provider'
 
@@ -8,20 +9,30 @@ require_relative 'provider/file_provider'
 class Application
 
   DEFAULT_PROVIDER=FileProvider
+  DEFAULT_SOLVER=BacktrackingSolver
 
   # Runs the #Application by getting the #BoardState from the #Provider and then having #Solver.solve interact with it
   #
   # @provider - The #Provider used to get the #BoardState
-  def self.run!(provider_type)
-    Solver.solve provider_type
+  def self.run!(provider_type, solver_type)
+    solver_type.new.solve provider_type
   end
 
-  # Returns a provider class based on name, if available.
+  # Returns a #Provider sub-type based on name, if available.
   #
-  # @provider_name - The name of the requested provider class.
+  # @provider_name - The name of the requested #Provider sub-type.
   def self.get_provider_type(provider_name)
     Provider.descendants.find do |clazz|
       clazz.provider_name == provider_name
+    end
+  end
+
+  # Returns a #Solver sub-type based on name, if available.
+  #
+  # @solver_name - The name of the requested #Solver sub-type.
+  def self.get_solver_type(solver_name)
+    Solver.descendants.find do |clazz|
+      clazz.solver_name == solver_name
     end
   end
 end
--- src/solver/solver.rb
+++ src/solver/solver.rb
@@ -1,87 +1,22 @@
-# #Solver is to be run with the options and solve a board.
+# A #Solver is any technique that takes in a #Provider, generates a #BoardState from it, and then using the
+# #BoardState and #Provider, solves the Latin square by attempting to produce a sudoku valid board from it.
 class Solver
 
-  # Determines if a #BoardState has a valid sudoku and generates a #BoardState containing that data.
+  # Default #solver_name is 'solver'
+  def self.solver_name
+    'solver'
+  end
+
+  # Determines if a #BoardState has a valid sudoku configuration and generates a #BoardState containing that data.
   # The #BoardState is retrieved from the provider.
   #
   # @provider_type - A #Provider type that supplies a #BoardState.
-  def self.solve(provider_type)
-    @provider = provider_type.new
-
-    # Solve the puzzle
-    board_state, result = solve_sudoku(@provider.provide_board_state)
-
-    # The resulting #BoardState and #Provider will be returned.
-    return @provider, board_state, result
+  def solve(provider_type)
   end
 
-  private
-
-  # Solves sudoku. Returns the resulting #BoardState.
-  #
-  # @board_state - The current board state to check
-  def self.solve_sudoku(board_state)
-    # If there is no unassigned location, we are done
-    if (@provider.filled? board_state)
-      return board_state, true # The #board_state is complete. Done!
-    end
-
-    column, row = find_next_unassigned_location(board_state)
-
-    # Iterate through assignable values.
-    @provider.assignable_values.each do |num|
-
-      # If #num is not present at the unassigned #row and #column...
-      if (is_safe?(board_state, column, row, num))
-
-        # Make tentative assignment.
-        updated_board_state = board_state.update(column, row, num)
-
-        # Try and solve the next step. If this returns true,
-        # the #board_state is complete and we have succeeded.
-        result_board_state, operation_result = solve_sudoku(updated_board_state)
-
-        # If it succeeded, return the board_state
-        if (operation_result)
-          return result_board_state, true
-        else
-          next
-        end
-      end
-    end
-
-    # This triggers backtracking.
-    return board_state, false
+  # Gets all descendants of the #Solver object.
+  def self.descendants
+    ObjectSpace.each_object(Class).select { |klass| klass < self }
   end
 
-  # Returns true if the #board_state can be assigned the #num value at #row and #column.
-  # This is possible if that number does not appear in that row, column, or 3x3 subsection
-  # corresponding to that location
-  #
-  # @board_state - The #BoardState instance whose safety is being checked.
-  # @column - The column number to test for insertion safeness.
-  # @row - The row number to test for insertion safeness.
-  # @num - The number to insert into the board at (row, column).
-  def self.is_safe?(board_state, column, row, num)
-    if( !board_state.row(row).contains?(num) && # If the row doesn't have the value...
-        !board_state.column(column).contains?(num) && # ...and the column doesn't have the value...
-        !board_state.zone_at(column, row).contains?(num)) # ...and the zone doesn't have the value.
-      true
-    else
-      false
-    end
-  end
-
-  # Returns the next (row, column) pair that do not have a value assigned to them.
-  #
-  # @board_state - The board state to check
-  def self.find_next_unassigned_location(board_state)
-    board_state.each_cell do |column, row|
-      if @provider.is_value_unassigned?( board_state.at(column, row) )
-        return column, row
-      end
-    end
-
-    raise 'Board is complete, no empty locations could be found.'
-  end
 end
