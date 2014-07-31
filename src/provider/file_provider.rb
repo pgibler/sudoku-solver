fcc2b3be3025de1a3aea54e5ce22bf9258b45393
Paul Gibler <paul.gibler@gmail.com>
2014-07-31 16:23:50 -0400

    Added in updated provider files.
    Provider now has all necessary functions in place.
    
    FileProvider is now fully functional and inherits from Provider.

--- src/provider/file_provider.rb
+++ src/provider/file_provider.rb
@@ -1,5 +1,9 @@
+# #BoardState is needed for generation.
+
+require_relative '../data/board_state'
+
 # #FileProvider will read in the command line options, find the file as the first argument (i.e. #ARGV[0]),
-# and then read through the sudoku puzzles 9x9 lines and generate a puzzle.
+# and then read through the sudoku boards 9x9 lines and generate a board.
 # Can read in two formats:
 # - spaced lines:
 #    4 0 0 5 2 0 1 0 3
@@ -23,24 +27,64 @@
 #    020000405
 #
 # It will automatically detect the format and parse correctly.
-class FileProvider
+class FileProvider < Provider
+
+  # Constants
+
+  UNASSIGNED_VALUE=0
 
   # Provider name is 'file'
-  def class.provider_name
+  def self.provider_name
     'file'
   end
 
-  # Provides a #BoardState by reading through a file, line-by-line, removing all white text, and then converting the lines
-  # into an array.
-  def provide_puzzle_state
-    file = ARGV[0]
-    board_array = []
+  # Provides a #BoardState by reading through a file, line-by-line, removing all white text, and then converting the
+  # lines into an array.
+  def provide_board_state
+    filename = ARGV[0]
 
-    File.open(file).each do |line|
-      board_array << convert_line_to_array(line)
+    if(filename.nil? || filename.strip == '')
+      raise 'File cannot be blank. Include a filename as the first argument for the program.'
+    end
+
+    board_array = Array.new(9)
+    board_array = board_array.map { Array.new(9) }
+    current_row = 0
+
+    File.open(filename).each do |line|
+      convert_line_to_array(line).each_with_index do |cell, current_column|
+        # Convert all cells into integers from strings.
+        board_array[current_column][current_row] = cell.to_i
+      end
+
+      current_row += 1
     end
 
     return BoardState.new(board_array)
+  end
+
+  # Determines to see if a cell contains 0 or not.
+  #
+  # @value - The value of the cell being checked.
+  def is_value_unassigned?(value)
+    value == UNASSIGNED_VALUE
+  end
+
+  # Returns #true if the #board_state is filled. Otherwise, #false.
+  #
+  # @board_state - The #BoardState whose filled status is being determined.
+  def filled?(board_state)
+    board_state.each_cell do |column, row|
+      return false if is_value_unassigned?( board_state.at(column, row) )
+    end
+
+    # If there are no more 0 numbers, return #true.
+    true
+  end
+
+  # Returns an #Array containing the values that could be assigned to a cell.
+  def assignable_values
+    (1..9)
   end
 
   private
@@ -52,7 +96,7 @@ class FileProvider
   #
   # @line - The line that is parsed into an array.
   def convert_line_to_array(line)
-    line.gsub(/\s+/, '').to_a
+    line.scan /\w/
   end
 
 end
--- src/provider/provider.rb
+++ src/provider/provider.rb
@@ -2,8 +2,34 @@
 # Some examples of potential providers include reading in from a file, socket connection, or the command line.
 # This default one provides provides nothing.
 class Provider
+
+  # Default #provider_name is 'provider'
+  def self.provider_name
+    'provider'
+  end
+
   # The provide method
-  def provide_puzzle_state
+  def provide_board_state
+
+  end
+
+  # Determines if a cell is empty or not based on its value.
+  #
+  # @value - The value of the cell being checked.
+  def is_value_unassigned?(value)
+
+  end
+
+  # Returns #true if the #board_state is filled. Otherwise, #false.
+  #
+  # @board_state - The #BoardState whose filled status is being determined.
+  def filled?(board_state)
+
+  end
+
+  # Returns an #Array containing the values that could be assigned to a cell given this provider.
+  # Unless the #Provider sub-type does something out of the ordinary, this will usually return (1..9).
+  def assignable_values
 
   end
 
