682139da2cfacfb69677b3e3aa4453ca07cc1484
giblaz <paul.gibler@gmail.com>
2014-08-02 02:13:12 -0400

    Cleared up syntax and improved documentation.

--- src/stdlib/class.rb
+++ src/stdlib/class.rb
@@ -1,10 +1,10 @@
 ##
-# Add to the base Ruby {Class}
+# Patch the Ruby base Class
 class Class
 
-  # Goes through each sub-type deriving from {Class} and returns them as an Array.
+  # Goes through each sub-type deriving from Class and returns them as an Array.
   #
-  # @return [Array] Returns the descendant sub-types of the {Class}.
+  # @return [Array] Returns the descendant sub-types of the Class.
   def descendants
     ObjectSpace.each_object(Class).select { |klass| klass < self }
   end
